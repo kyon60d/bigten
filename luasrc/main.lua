ab03c09bbcf23d218337bcc61744ccf2992412b0
QigaoLiu <QigaoLiu@boyaa.com>
2016-09-29 19:09:58 +0800

    删除无用文件

--- luasrc/main.lua
+++
@@ -1,6 +0,0 @@
-
-cc.FileUtils:getInstance():setPopupNotify(false)
-cc.FileUtils:getInstance():addSearchPath("G:/work/cocos/bigten/luares/")
-package.path = "G:/work/cocos/bigten/luasrc/"
-
-require "mainlua"
